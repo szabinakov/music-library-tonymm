123469abfa86d8299b4f4ae856fc69300eda1cec
TonyMM55 <tonymmoran@gmail.com>
2023-02-20 09:07:08 +0000

    modified controller code to be more secure

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -5,7 +5,7 @@ const createArtist = async (req, res) => {
   const { name, genre } = req.body
 
   try {
-    const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+    const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
     res.status(201).json(artist)
   } catch (err) {
     res.status(500).json(err.message)
