e6eb20f0ffae05407b5a668fc55bc8e4c4bd1c26
TonyMM55 <tonymmoran@gmail.com>
2023-02-20 12:40:52 +0000

    Added 2 more tests getAllArtists, getArtistById

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -12,4 +12,30 @@ const createArtist = async (req, res) => {
   }
 };
 
-module.exports = { createArtist } 
+const getAllArtists = async (_, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM Artists')
+    res.status(200).json(rows)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}; 
+
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { rows: [ artist ] } = await db.query('SELECT * FROM Artists WHERE id = $1', [id])
+
+    if (!artist) {
+      res.status(404).json({ message: `artist ${id} does not exist` })
+    }
+    res.status(200).json(artist)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}; 
+
+
+
+
+module.exports = { createArtist, getAllArtists, getArtistById } 
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,5 +5,7 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.createArtist);
+router.get('/', artistController.getAllArtists); 
+router.get('/:id', artistController.getArtistById);
 
 module.exports = router;
